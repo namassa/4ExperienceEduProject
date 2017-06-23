bab75a7b306370865f6ed299f38cb451840845d8
DSiemienik <dsiemienik@gmail.com>
2017-06-23 07:59:59 +0200

    Async scene loading

---
+++ Assets/Scripts/Engine/AppController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// karol@4experience.co
+// core controller of the app
+public class AppController : MonoBehaviour {
+	
+	// simple event
+	public System.Action onInitialized = delegate { };
+
+	// half singleton
+	public static AppController Instance { get; private set; }
+	void Awake() {
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		//
+		LoadSetup ();
+	}
+
+	//
+	void LoadSetup() {
+		#if LOG_ENGINE
+		Debug.Log("AppController : Load Specific Setup");
+		#endif
+
+		//
+		StartCoroutine (LoadSetupCoroutine ());
+	}
+
+	//
+	IEnumerator LoadSetupCoroutine() {
+
+		#if LOG_ENGINE
+		Debug.Log("AppController : Load Async Setup");
+		#endif
+
+		// TU POWINNO SIE CALY ASYNC TRZYMAC W LADOWANIU
+
+		yield return new WaitForEndOfFrame ();
+		// 
+		OnInitialized ();
+
+		#if LOG_ENGINE
+		Debug.Log("AppController : Load Menu Scene");
+		#endif
+
+		SceneLoadingController.Instance.LoadMainScene(GameScenes.Menu);
+	}
+		
+	//
+	void OnInitialized() {
+		#if LOG_ENGINE
+		Debug.Log("AppController : Call OnInitialized Event");
+		#endif
+
+		onInitialized ();
+	}
+}
---
+++ Assets/Scripts/Engine/SceneLoadingController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameScenes { Menu } 
+
+// karol@4experience.co
+// responsible for changing scenes and broadcasting progress and isDone events
+public class SceneLoadingController : MonoBehaviour {
+
+	// half singleton
+	public static SceneLoadingController Instance { get; private set; }
+
+	void Awake() {
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+	}
+
+	//
+	public bool IsLoading { get; private set; }
+	public float Progress { get; private set; }
+	public System.Action<GameScenes> onLoadingComplete = delegate { };
+	public System.Action<GameScenes> onLoadingBegin = delegate { };
+
+	//
+	void Start() {
+		IsLoading = false;
+		Progress = 0f;
+	}
+		
+	//
+	public void LoadMainScene(GameScenes scene) {
+        // TODO Code scene loading with unloading current scene
+        StartCoroutine(SceneLoad(GameScenes.Menu));
+	}
+    public void LoadScene(GameScenes scene, bool withUnload)
+    {
+        StartCoroutine(SceneLoad(scene, withUnload));
+    }
+
+    IEnumerator SceneLoad(GameScenes scene, bool withUnload = false)
+    {
+        if(withUnload)
+        {
+            Scene sceneToUnload = SceneManager.GetActiveScene();
+            AsyncOperation unloadScene = SceneManager.UnloadSceneAsync(sceneToUnload.name);
+            while (!unloadScene.isDone)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        
+        string sceneToLoad = GetSceneName(scene);
+
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+
+        while(!loading.isDone)
+        {
+            Progress = loading.progress;
+            yield return new WaitForEndOfFrame();
+        }
+
+        Scene loadedScene = SceneManager.GetSceneByName(sceneToLoad);
+        SceneManager.SetActiveScene(loadedScene);
+    }
+
+	//
+	private string GetSceneName(GameScenes scene) {
+		switch (scene) {
+		case GameScenes.Menu:
+			return "002_Menu";
+            default:
+			return string.Empty;
+		}
+	}
+}
