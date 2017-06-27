58564ef95be4cbf9b1455e79e7bbfe8f2e2e7182
DSiemienik <dsiemienik@gmail.com>
2017-06-27 12:48:34 +0200

    Unfeedback async scene loading

---
+++ Assets/Scripts/Engine/GameScenes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// karol.ryt@gmail.com
+// we count only gameplay viable scenes
+public enum GameScene {
+	Menu // 002_Menu
+}
