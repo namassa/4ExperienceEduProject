b4d38901a84f189d87775b5ab0c85cb11b6cc895
Jarek <jarekdc@gmail.com>
2017-06-27 12:52:42 +0200

    applied feedback

--- Assets/Scripts/Engine/SceneLoadingController.cs
+++ Assets/Scripts/Engine/SceneLoadingController.cs
@@ -3,33 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-// FEEDBACK:
-// karol.ryt@gmail.com
-//  1. wywal enuma;) fajne ze jest ale bedziemy tylko robic coroutine
-//  2. popraw kod tak by enumow nie bylo
-//  3. to aktywowanie scen na spacji tez bym olal:)
-//  4. jak masz takie cos jak w 42 lini z System.Action<> nazwaeventa = delegate { }; to nie potrzeba sprawdzac w twoich funkacjach
-//	   czy istnieja eventy a po prosty wolac onLoadingBegin(scena); To tez wiec sobie skoryguj i daj do coroutyny:) tak by o 2 funkcje mniej miec.
-//  5. Debug.Logi wsadzic w #if Log_Engine albo wywalic;)
-//  6. podpisujemy sie z data ponizej mojej usunietej stopki tak by wiadomo kto zmienial;)
+// jarekdc@gmail.com 27.06.2017
+// removed unnecessary enum and functions, put debug into #if Log_Engine
 
 // karol@4experience.co
 // responsible for changing scenes and broadcasting progress and isDone events
 public class SceneLoadingController : MonoBehaviour {
-
-    enum SceneLoadingMode {
-        Unity_LoadSceneSingle,
-        Unity_LoadSceneAdditive,
-        Unity_LoadSceneAsyncSingle,
-        Unity_LoadSceneAsyncAdditive,
-        Custom_AsyncLoad
-    }
-
-	// karol
-	// z racji wyzej tego nam nie potrzeba
-    [SerializeField]
-    private SceneLoadingMode sceneLoadingMode = SceneLoadingMode.Custom_AsyncLoad;
 
 	// half singleton
 	public static SceneLoadingController Instance { get; private set; }
@@ -52,33 +31,10 @@ public class SceneLoadingController : MonoBehaviour {
 
 	//
 	public void LoadScene(GameScene scene, bool unloadCurrent) {
-        // TODO Code scene loading with unloading current scene
-
         if (IsLoading) {
-            Debug.Log("Already loading a scene!");
+			Debug.LogWarning("Already loading a scene!");
         } else {
-            switch (sceneLoadingMode)
-            {
-                case SceneLoadingMode.Unity_LoadSceneSingle:
-                    SceneManager.LoadScene(GetSceneName(scene), LoadSceneMode.Single);
-                    break;
-
-                case SceneLoadingMode.Unity_LoadSceneAdditive:
-                    SceneManager.LoadScene(GetSceneName(scene), LoadSceneMode.Additive);
-                    break;
-
-                case SceneLoadingMode.Unity_LoadSceneAsyncSingle:
-                    SceneManager.LoadSceneAsync(GetSceneName(scene), LoadSceneMode.Single);
-                    break;
-
-                case SceneLoadingMode.Unity_LoadSceneAsyncAdditive:
-                    SceneManager.LoadSceneAsync(GetSceneName(scene), LoadSceneMode.Additive);
-                    break;
-
-                case SceneLoadingMode.Custom_AsyncLoad:
-                    StartCoroutine(Custom_AsyncLoadCor(scene, unloadCurrent));
-                    break;
-            }
+			StartCoroutine(Custom_AsyncLoadCor(scene, unloadCurrent));
         }
 	}
 
@@ -99,7 +55,9 @@ public class SceneLoadingController : MonoBehaviour {
         yield return new WaitForSeconds (1f);
 
         Scene currScene = SceneManager.GetActiveScene();
-        Debug.Log(currScene.name);
+		#if LOG_ENGINE
+        Debug.Log("Current scene: " + currScene.name);
+		#endif
 
         AsyncOperation asyncLoadOp;
 
@@ -115,7 +73,8 @@ public class SceneLoadingController : MonoBehaviour {
         asyncLoadOp = SceneManager.LoadSceneAsync(GetSceneName(scene), LoadSceneMode.Additive);
         asyncLoadOp.allowSceneActivation = false;
 
-        HandleLoadingStart(scene);
+		onLoadingBegin(scene);
+		IsLoading = true;
 
         while (!asyncLoadOp.isDone) {
             // unity's loading progress is clamped between 0-0.9
@@ -123,40 +82,17 @@ public class SceneLoadingController : MonoBehaviour {
 
             // convert progress value to 0-1
             Progress = Mathf.Clamp01(asyncLoadOp.progress / 0.9f);
+			#if LOG_ENGINE
             Debug.Log("Loading progress: " + (Progress * 100) + "%");
+			#endif
 
             if (asyncLoadOp.progress == 0.9f) {
-                Debug.Log("Press space to activate loaded scene");
-                if (Input.GetKeyDown(KeyCode.Space)) {
-                    asyncLoadOp.allowSceneActivation = true;
-
-                    HandleLoadingFinished(scene);
-                }
+                asyncLoadOp.allowSceneActivation = true;
+				onLoadingComplete(scene);
+				IsLoading = false;
             }
 
             yield return null;
         }
-    }
-
-    //
-    void HandleLoadingStart(GameScene scene)
-    {
-        if (onLoadingBegin != null)
-        {
-            onLoadingBegin(scene);
-        }
-
-        IsLoading = true;
-    }
-
-    //
-    void HandleLoadingFinished(GameScene scene)
-    {
-        if (onLoadingComplete != null)
-        {
-            onLoadingComplete(scene);
-        }
-
-        IsLoading = false;
     }
 }
