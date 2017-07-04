1e6bda7c2fef7141f0dd7a2e7447b358041c5e51
Jarek <jarekdc@gmail.com>
2017-07-04 21:28:10 +0200

    +RandomizePositionCommand

---
+++ Assets/Scripts/Gameplay/RandomizePosititonCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// jarekdc@gmail.com
+// Holds an object and information about a potential collission
+public class RandomizePosititonCommand
+{
+    GameObject sender;
+    Vector2 collisionDirection;
+}
