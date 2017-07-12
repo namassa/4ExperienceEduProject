d054401e839f8c4132b3a44f153467c37de83dbc
Jarek <jarekdc@gmail.com>
2017-07-12 14:01:31 +0200

    +spawn buttons

--- Assets/Scripts/DEBUG/DEBUG_InputController.cs
+++ Assets/Scripts/DEBUG/DEBUG_InputController.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // jarek@gmail.com
-// Create spawn commands and passes them to the FactoryController
+// Creates spawn commands and passes them to the FactoryController
 // Command patterns are exposed to the inspector
 public class DEBUG_InputController : MonoBehaviour 
 {
@@ -12,11 +13,23 @@ public class DEBUG_InputController : MonoBehaviour
 	private class ButtonSpawnCmdBind
 	{
 		public KeyCode keyCode;
+		public Button button;
 		public SpawnCommand cmd;
 	}
 
+	[Header("Key Press Commands")]
 	[SerializeField]
 	private ButtonSpawnCmdBind[] spawnCmd;
+
+	//
+	void Awake ()
+	{
+		foreach (ButtonSpawnCmdBind cmdBind in spawnCmd) 
+		{
+			cmdBind.button.onClick.AddListener (() => EnemyFactoryController.Instance.PassSpawnCommand (new SpawnCommand (cmdBind.cmd.enemyPrefabName)));
+			cmdBind.button.GetComponentInChildren<Text> ().text = "Spawn " + cmdBind.cmd.enemyPrefabName;
+		}
+	}
 
 	//
 	void Update () 
